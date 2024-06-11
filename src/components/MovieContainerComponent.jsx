e8aa99d3142f4f7e53c831e9ac3bddee05c7ac37
wisnu andika <wisnuandika104@gmail.com>
2024-06-11 15:30:07 +0700

    feat: add api data fetching and display popular movies data

--- src/components/MovieContainerComponent.jsx
+++ src/components/MovieContainerComponent.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getMovieList } from '../api/api'
 
 export default function MovieContainerComponent() {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
   return (
-    <div className="w-full bg-transparent mt-5 p-5 border border-white border-x-transparent border-y-gradient-to-r from-blue-800 to-blue-500">
+    <div className="w-full flex flex-wrap items-center justify-center gap-4 bg-transparent mt-5 p-5 border border-white border-x-transparent border-y">
       {/* example card 1 */}
-      <div className="w-[400px] max-h-[500px] border border-white p-4 rounded-xl">
-        <div className="flex flex-col p-4 gap-y-3 items-center justify-center">
-          <img src="https://placehold.co/300x300" alt="" />
-          <div className="flex flex-col gap-y-3 text-center">
-            <h1 className="text-3xl font-bold tracking-wide">Title data 1</h1>
-            <p className="text-md font-semibold text-neutral-500">release data 1</p>
-            <p className="text-xl font-bold text-yellow-500">rate data 1</p>
+      {popularMovies.map((movie, index) => (
+        <div key={index} className="w-[400px] min-h-[500px] border border-white p-4 rounded-xl">
+          <div className="flex flex-col p-4 gap-y-3 items-center justify-center">
+            <img src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} alt="" />
+            <div className="flex flex-col gap-y-3 text-center">
+              <h1 className="text-3xl font-bold tracking-wide min-h-20">{movie.title}</h1>
+              <p className="text-md font-semibold text-neutral-500">Release date: {movie.release_date}</p>
+              <p className="text-xl font-bold text-yellow-500">Average vote: {movie.vote_average}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   )
 }
--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { searchMovie } from '../api/api'
 
 export default function SearchComponent() {
+  let [value, setValue] = useState('')
+
+  const setValueFunction = (keyword) => {
+    setValue(keyword)
+  }
+
+  const search = async(q) => {
+    if(q.length > 3) {
+      const query = await searchMovie(q)
+      // setPopularMovies(query.results)
+      console.log(query)
+    }
+  }
+
   return (
-    <div className="w-full mt-2 p-3 flex items-center justify-center">
-      <input className="bg-neutral-700 border border-white border-dotted p-3 rounded-md text-blue-100 animate-pulse focus:border-white focus:animate-none" type="text" size={30} placeholder="Type your movie title here" />
+    <div className="w-full mt-2 p-3 flex flex-col gap-y-3 items-center justify-center">
+      <input className="bg-neutral-700 border border-white border-dotted p-3 rounded-md text-blue-100 animate-pulse focus:border-white focus:animate-none" type="text" size={30} placeholder="Type your movie title here"
+        onChange={({ target }) => {
+          search(target.value)
+          setValueFunction(target.value)
+        }} />
+      <p className="font-bold">Search: {" "}
+        <span className="text-blue-800">
+          {value}
+        </span>
+      </p>
     </div>
   )
 }
